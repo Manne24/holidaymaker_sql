009f5eee4332ef5be218675df93eb79f7ecf8cf3
Emmanuel Ndziba <Emmanuel_Ndziba@hotmaill.se>
2020-02-19 12:56:38 +0100

    v2 test

--- src/com/holidaymaker/Main.java
+++ src/com/holidaymaker/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Test Git!!! ");
+        System.out.println("Test Git!!! v2 ");
     }
 }
